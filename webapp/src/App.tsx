09a6f6655c4434e7bfe62e41fe6a2c7d6fb784e2
MaxWebDev <maximus3506@mail.ru>
2024-12-21 22:01:25 +0300

    add prettier and update some code

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
+  type Idea = {
+    id: number
+    title: string
+    text: string
+  }
+  const ideas: Idea[] = [
+    {
+      id: 1,
+      title: 'Idea 1',
+      text: 'Text for idea 1',
+    },
+    {
+      id: 2,
+      title: 'Idea 2',
+      text: 'Text for idea 2',
+    },
+    {
+      id: 3,
+      title: 'Idea 3',
+      text: 'Text for idea 3',
+    },
+    {
+      id: 4,
+      title: 'Idea 4',
+      text: 'Text for idea 4',
+    },
+    {
+      id: 5,
+      title: 'Idea 5',
+      text: 'Text for idea 5',
+    },
+  ]
   return (
-    <>
+    <div>
+      <h1>Ideas App</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {ideas.map((idea) => (
+          <div key={idea.id}>
+            <h2>{idea.title}</h2>
+            <p>{idea.text}</p>
+          </div>
+        ))}
       </div>
-      <h1>MyApp</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
