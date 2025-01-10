95a47339747baf226e58c0959e1e0a1254415f99
madmax <m.maltsev@dsec.ru>
2025-01-10 23:39:13 +0300

    add routes types

--- webapp/src/lib/routes.ts
+++ webapp/src/lib/routes.ts
@@ -1,2 +1,13 @@
+
+export const getRouteParams = <T extends Record<string, boolean>>(params: T) => {
+    return Object.keys(params).reduce((acc, key) => ({ ...acc, [key]: `:${key}` }), {}) as Record<keyof T, string>
+}
+
+
 export const getAllIdeasRoute = () => '/'
-export const getIdeaRoute = (id: string) => `/idea/${id}`
+
+export const viewIdeaRouteParams = getRouteParams({ id: true })
+export type ViewIdeaRouteParams = typeof viewIdeaRouteParams
+export const getViewIdeaRoute = ({id}: ViewIdeaRouteParams) => `/idea/${id}`
+
+// export const getIdeaRoute = (id: string) => `/idea/${id}`
--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 
 export const ViewIdeaPage = () => {
-  const { id } = useParams()
+  const { id } = useParams() as { id: string }
   return (
     <div>
       <h1>View Idea { id }</h1>
